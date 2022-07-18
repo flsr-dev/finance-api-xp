23727c486f78d1d6373059c86dae71bbc73d4830
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-18 12:39:04 -0300

    feat: create auth middleware

--- src/utils/error.messages.js
+++ src/utils/error.messages.js
@@ -1,3 +1,4 @@
 module.exports = {
   INTERNAL_ERROR_MSG: { message: 'Internal server error' },
+  AUTHENTICATION_ERROR_MSG: { message: 'Invalid or not found token' },
 };
---
+++ src/utils/jwt.js
@@ -0,0 +1,31 @@
+const Jwt = require('jsonwebtoken');
+const { StatusCodes } = require('http-status-codes');
+const HttpException = require('../classes/http.exception');
+const { AUTHENTICATION_ERROR_MSG } = require('./error.messages');
+
+const { JWT_SECRET } = process.env;
+
+const jwtConfig = {
+  // expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const createToken = (payload) => Jwt.sign(payload, JWT_SECRET, jwtConfig);
+
+const authenticateToken = (token) => {
+  if (!token) {
+    throw new HttpException(StatusCodes.UNAUTHORIZED, AUTHENTICATION_ERROR_MSG);
+  }
+
+  try {
+    const authenticate = Jwt.verify(token, JWT_SECRET);
+    return authenticate;
+  } catch (error) {
+    throw new HttpException(StatusCodes.UNAUTHORIZED, AUTHENTICATION_ERROR_MSG);
+  }
+};
+
+module.exports = {
+  createToken,
+  authenticateToken,
+};
