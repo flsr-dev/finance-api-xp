e742ac221ab46cdbccfcd37b0e96d3b488833f31
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-18 11:59:47 -0300

    chore: create error middleware

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,12 @@
+const { StatusCodes } = require('http-status-codes');
+const errorMessages = require('../utils/error.messages');
+
+const httpErrorMiddleware = (err, _req, res, _next) => {
+  const { status, message } = err;
+  if (status) {
+    return res.status(status).json({ message });
+  }
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorMessages.INTERNAL_ERROR_MSG);
+};
+
+module.exports = httpErrorMiddleware;
---
+++ src/utils/error.messages.js
@@ -0,0 +1,3 @@
+module.exports = {
+  INTERNAL_ERROR_MSG: { message: 'Internal server error' },
+};
