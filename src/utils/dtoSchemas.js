b4eeb0aa08cbe9926688017db173c7c12986f341
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-20 20:55:55 -0300

    refactor: change joi schemas to reutilize objects and create validations for conta route

--- src/utils/dtoSchemas.js
+++ src/utils/dtoSchemas.js
@@ -1,32 +1,33 @@
 const Joi = require('joi');
 
+const investimentsObjectSchema = Joi
+  .object(
+    {
+      codCliente: Joi.number().required(),
+      codAtivo: Joi.number().required(),
+      qtdeAtivo: Joi.number().required().min(1),
+    },
+  ).messages({
+    'any.required': '{{#label}} is required',
+    'number.min': '{{#label}} must be greater than or equal to {{#limit}}',
+  });
+
+const accountObjectSchema = Joi
+  .object({
+    codCliente: Joi.number().required(),
+    valor: Joi.number().required().greater(0).precision(2),
+  }).messages({
+    'any.required': '{{#label}} is required',
+    'number.greater': '{{#label}} must be greater than {{#limit}}',
+    'number.precision': '{{#label}} must have 2 decimal places',
+  });
+
 module.exports = {
 
-  investimentosComprar: Joi
-    .object(
-      {
-        codCliente: Joi.number().required(),
-        codAtivo: Joi.number().required(),
-        qtdeAtivo: Joi.number().required().min(1),
-      },
-    )
-    .messages({
-      'any.required': '{{#label}} is required',
-      'number.min': '{{#label}} must be greater than or equal to 1',
-    }),
-
-  investimentosVender: Joi
-    .object(
-      {
-        codCliente: Joi.number().required(),
-        codAtivo: Joi.number().required(),
-        qtdeAtivo: Joi.number().required().min(1),
-      },
-    )
-    .messages({
-      'any.required': '{{#label}} is required',
-      'number.min': '{{#label}} must be greater than or equal to 1',
-    }),
+  investimentosComprar: investimentsObjectSchema,
+  investimentosVender: investimentsObjectSchema,
+  contaDeposito: accountObjectSchema,
+  contaSaque: accountObjectSchema,
 
   clientesAtivos: Joi.object({
     codCliente: Joi.number().required(),
@@ -39,4 +40,5 @@ module.exports = {
   }).messages({
     'any.required': '{{#label}} is required',
   }),
+
 };
