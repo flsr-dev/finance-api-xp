d290626f9fccea2a80ec6728bf6fe627501ce151
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-22 11:48:24 -0300

    refactor: move database transaction configuration to controller

--- src/services/investimentos.services.js
+++ src/services/investimentos.services.js
@@ -1,14 +1,10 @@
-const Sequelize = require('sequelize');
 const { StatusCodes } = require('http-status-codes');
 const { Ativos, Transacoes, AtivosClientes } = require('../database/models');
-const config = require('../database/config/config');
 const HttpException = require('../classes/http.exception');
 const { ASSET_NOT_FOUND_MSG } = require('../utils/errorMessages');
 const { isAssetEnough } = require('../utils/isAssetEnough');
 const { calcBrokerAmount, calcUserAmount } = require('../utils/calculateAssetAmount');
 const transactionCodes = require('../utils/transactionCodes');
-
-const sequelize = new Sequelize(config.production);
 
 const createBuyTransaction = async (
   requestedAsset,
@@ -63,25 +59,23 @@ const updateOrCreateClientAsset = async (
   );
 };
 
-const operateAsset = async (body, operationType) => {
+const operateAsset = async (body, operationType, seqTransaction) => {
   const { codAtivo } = body;
-  return sequelize.transaction(async (t) => {
-    const brokerAssetData = await Ativos.findByPk(body.codAtivo);
-    if (!brokerAssetData) {
-      throw new HttpException(StatusCodes.NOT_FOUND, ASSET_NOT_FOUND_MSG);
-    }
-    const isAssetAvailable = await isAssetEnough(body, brokerAssetData, operationType);
 
-    if (isAssetAvailable) {
-      const { qtdeAtivo: brokerStoredAsset } = brokerAssetData;
-      const newAssetAmount = calcBrokerAmount(brokerStoredAsset, body.qtdeAtivo, operationType);
-      await Ativos.update({ qtdeAtivo: newAssetAmount }, { where: { codAtivo }, transaction: t });
-      await updateOrCreateClientAsset(body, brokerAssetData, operationType, t);
-      return createBuyTransaction(body, brokerAssetData, operationType, t);
-    }
+  const brokerAssetData = await Ativos.findByPk(body.codAtivo);
+  if (!brokerAssetData) {
+    throw new HttpException(StatusCodes.NOT_FOUND, ASSET_NOT_FOUND_MSG);
+  }
+  const isAssetAvailable = await isAssetEnough(body, brokerAssetData, operationType);
 
-    return false;
-  });
+  if (isAssetAvailable) {
+    const { qtdeAtivo: brokerStoredAsset } = brokerAssetData;
+    const newAssetAmount = calcBrokerAmount(brokerStoredAsset, body.qtdeAtivo, operationType);
+    await Ativos.update({ qtdeAtivo: newAssetAmount }, { where: { codAtivo }, transaction: seqTransaction });
+    await updateOrCreateClientAsset(body, brokerAssetData, operationType, seqTransaction);
+    return createBuyTransaction(body, brokerAssetData, operationType, seqTransaction);
+  }
+  return false;
 };
 
 module.exports = {
