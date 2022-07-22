e6a3463074e683993a92ab08be9cb53c063c0fcf
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-22 10:43:03 -0300

    test: create contas.services tests

--- tests/mocks/ContasData.js
+++ tests/mocks/ContasData.js
@@ -1,15 +1,15 @@
-const foundClient = {
+const { stub } = require('sinon');
+
+const clientAccount = {
+  codConta: 1,
   codCliente: 1,
-  nome: 'John',
-  sobrenome: 'Doe',
-  email: 'johndoe@email.com',
-  senha: 'senhatemporaria',
-  cpf: '12345677785',
+  valor: 0.00,
   ativo: true,
   criacao: new Date('2011-08-01T19:58:00.000Z'),
   atualizacao: new Date('2011-08-01T19:58:00.000Z'),
+  save: stub(),
 };
 
 module.exports = {
-  foundClient,
+  clientAccount,
 };
---
+++ tests/mocks/OperacoesData.js
@@ -0,0 +1,10 @@
+const newOperation = {
+  codCliente: 1,
+  codTipoOperacao: 1,
+  valor: 20000.00,
+  criacao: new Date('2011-08-01T19:58:00.000Z'),
+};
+
+module.exports = {
+  newOperation,
+};
