8331126700574baf756e14568625abb8b799c0f2
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-22 10:51:24 -0300

    refactor: change getAccount function name and change the expected id variable name

--- src/services/contas.services.js
+++ src/services/contas.services.js
@@ -4,8 +4,8 @@ const { Contas, Operacoes } = require('../database/models');
 const { CLIENT_NOT_FOUND_MSG } = require('../utils/errorMessages');
 const operationCodes = require('../utils/operationCodes');
 
-const checkIfAccountExists = async (codConta) => {
-  const account = await Contas.findOne({ where: { codConta } });
+const getAccount = async (codCliente) => {
+  const account = await Contas.findOne({ where: { codCliente } });
   if (!account) {
     throw new HttpException(StatusCodes.NOT_FOUND, CLIENT_NOT_FOUND_MSG);
   }
@@ -21,17 +21,24 @@ const calcNewBalance = (account, valor, operationType) => {
 
 const createNewAccountOperation = (account, valor, operationType, seqTransaction) => {
   const codTipoOperacao = operationCodes[operationType];
-  const codConta = account.codCliente;
-  return Operacoes.create({ valor, codConta, codTipoOperacao }, { transaction: seqTransaction });
+  const { codCliente } = account;
+  return Operacoes.create({ valor, codCliente, codTipoOperacao }, { transaction: seqTransaction });
 };
 
 const accountBalanceOperation = async ({ codCliente, valor }, operationType, seqTransaction) => {
-  const account = await checkIfAccountExists(codCliente);
+  const account = await getAccount(codCliente);
   account.valor = calcNewBalance(account, valor, operationType);
   account.save({ transaction: seqTransaction });
   return createNewAccountOperation(account, valor, operationType, seqTransaction);
 };
 
+const getClientAccountBalance = async ({ codCliente }) => {
+  const account = await getAccount(codCliente);
+  return account;
+};
+
 module.exports = {
   accountBalanceOperation,
+  getClientAccountBalance,
+  calcNewBalance,
 };
