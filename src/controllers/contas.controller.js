7c6d8855137d4d60e289e0639cf3c510e85e797b
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-22 10:49:12 -0300

    fix: change the expected id variable name from payload to be "codCliente"

--- src/controllers/contas.controller.js
+++ src/controllers/contas.controller.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const { StatusCodes } = require('http-status-codes');
-const { accountBalanceOperation } = require('../services/contas.services');
+const { accountBalanceOperation, getClientAccountBalance } = require('../services/contas.services');
 const { INTERNAL_ERROR_MSG } = require('../utils/errorMessages');
 const config = require('../database/config/config');
 const toCamelCase = require('../utils/convertToCamelCase');
@@ -9,7 +9,7 @@ const { checkIfClientIsAuthorized } = require('../utils/checkClientAuthorization
 const sequelize = new Sequelize(config.production);
 
 const operateAccountBalance = async (req, res) => {
-  checkIfClientIsAuthorized(req.body.codCliente, req.user.id);
+  checkIfClientIsAuthorized(req.body.codCliente, req.user.codCliente);
   const operationType = toCamelCase(req.path);
   const client = await sequelize.transaction(async (t) => (
     accountBalanceOperation(req.body, operationType, t)
@@ -20,4 +20,13 @@ const operateAccountBalance = async (req, res) => {
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: INTERNAL_ERROR_MSG });
 };
 
-module.exports = { operateAccountBalance };
+const getClientBalance = async (req, res) => {
+  checkIfClientIsAuthorized(req.params.codCliente, req.user.codCliente);
+  const accountBalance = await getClientAccountBalance(req.params);
+  return res.status(StatusCodes.OK).json(accountBalance);
+};
+
+module.exports = {
+  operateAccountBalance,
+  getClientBalance,
+};
