edcb9c94b250821582e1799d5680cb0623608bfe
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-24 14:56:16 -0300

    chore: change sequelize seeders

--- src/database/seeders/20220718015234-contas.js
+++ src/database/seeders/20220718015234-contas.js
@@ -6,14 +6,14 @@ module.exports = {
       [
         {
           cod_cliente: 1,
-          valor: 0.00,
+          valor: 10000.00,
           ativo: true,
           criacao: new Date('2011-08-01T19:58:00.000Z'),
           atualizacao: new Date('2011-08-01T19:58:00.000Z'),
         },
         {
           cod_cliente: 2,
-          valor: 0.00,
+          valor: 1240.00,
           ativo: true,
           criacao: new Date('2011-08-01T19:58:00.000Z'),
           atualizacao: new Date('2011-08-01T19:58:00.000Z'),
