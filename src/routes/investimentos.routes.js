371f15a274cb8f2e1363031a3d9daf2c17e730d7
Fernando Ribeiro <flsr.dev@gmail.com>
2022-07-24 14:56:48 -0300

    fix: add authorization middleware to route investimentos.services

--- src/routes/investimentos.routes.js
+++ src/routes/investimentos.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const InvestimentosControllers = require('../controllers/investimentos.controller');
 const { authenticationMiddleware } = require('../middlewares/authentication.middleware');
 const requestValidationMiddleware = require('../middlewares/requestValidation.middleware');
+const { authorizationMiddleware } = require('../middlewares/authorization.middleware');
 
 const router = express.Router();
 
 router.use(authenticationMiddleware);
-router.post('/comprar', requestValidationMiddleware, InvestimentosControllers.operateAsset);
-router.post('/vender', requestValidationMiddleware, InvestimentosControllers.operateAsset);
+router.post('/comprar', requestValidationMiddleware, authorizationMiddleware, InvestimentosControllers.operateAsset);
+router.post('/vender', requestValidationMiddleware, authorizationMiddleware, InvestimentosControllers.operateAsset);
 
 module.exports = router;
